f1c04dd345f03e2d52c0c69040be4659e6a29a9d
Edgar Diaz <127860772+edgardiaz1212@users.noreply.github.com>
2025-04-10 06:56:45 -0400

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.BACKEND_URL || '/api'
-console.log(baseURL)
+
 // Crear una instancia de axios con configuración común
 const api = axios.create({
   baseURL: baseURL,
